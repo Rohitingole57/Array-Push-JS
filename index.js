333703716556cbbd670c52367437aa3693bd65f4
Rohit Ingole <ingolerohit2017@gmail.com>
2023-04-06 10:51:50 +0530

    array.push

---
+++ index.js
@@ -0,0 +1,28 @@
+//--------------------Arrays-------------------------
+
+var names = ["rohit","aniket","tai","mummy","papa"]
+console.log(names);
+console.log(names.length);
+var c= names.length - 2;
+console.log(c);
+var item = names.length
+console.log(names[item-2])
+names[1]= "egg";
+console.log(names);
+names.pop();
+console.log(names);
+console.log(names.length);
+
+
+
+var movies= []
+movies.push("rrr");
+movies.push("ttt");
+movies.push("yyy");
+movies.push("uuu");
+movies.push("iii");
+movies.push("ooo");
+console.log(movies);
+movies.pop()
+console.log(movies);
+
